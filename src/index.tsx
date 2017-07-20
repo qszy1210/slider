9dc0fa726e966b5db36525cc9980018d62936f99
北山秋叶 <asdfqs@163.com>
2017-07-20 09:36:11 +0800

    add npm & tsconfig

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export interface IndexProps {
+}
+
+class Index extends React.Component<IndexProps, any> {
+  render() {
+    return (
+      <div>
+        index config
+      </div>
+    );
+  }
+}
+
+export default Index;
