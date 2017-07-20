55f9a164f3f7a702c67f3f224b99199f8977dfbc
北山秋叶 <asdfqs@163.com>
2017-07-20 10:34:45 +0800

    change dir & finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: "./src/index.tsx",
     output: {
-        filename: "bundle.js"
+        filename: "dist/bundle.js"
     },
     resolve: {
         // Add '.ts' and '.tsx' as a resolvable extension.
